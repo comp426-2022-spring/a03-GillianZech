02c7af2a67e69d2e413eb663ceb2098d24d85455
Gillian Zech <ginger01@email.unc.edu>
2022-03-10 23:53:02 -0500

    starting setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+var port = 5000;
+
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%', port));
+})
+
+app.get('/app', (req, res) => {
+    res.status(200).end('OK, the API is working!');
+    res.type("text/plain");
+})
+
+app.get('/app/echo/:number', (req, res) => {
+    res.status(200).json({ 'message': req.params.number })
+})
+
+app.use(function(req, res) {
+    res.status(404).send("Endpoint does not exist");
+    res.type("text/plain");
+})
