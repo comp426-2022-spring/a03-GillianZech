fd48f3270bb29bee748e3d68d78862ac090dd74e
Gillian Zech <ginger01@email.unc.edu>
2022-03-17 17:24:37 -0400

    installed and required minimist

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const app = express();
+const args = require('minimist')(process.argv.slice(2));
+args['port_arg'];
+const port_arg = args.port_arg;
 
-var port = 5000;
+if (port_arg == null) {
+    var port = 5000;
+} else {
+    var port = port_arg;
+}
 
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port));
 })
 
-app.get('/app/', (req, res) => {
-    res.status(200).end('OK, the API is working!');
+app.get('/app', (req, res) => {
+    res.status(200).end('200 OK');
     res.type("text/plain");
 })
 
 app.get('/app/flips/:number', (req, res) => {
-    const flips = manyflips(req.params.number)
-    res.status(200).json({ 'message': req.params.number })
+    // const flips = manyflips(req.params.number)
+    // res.status(200).json({ 'message': req.params.number })
+    res.status(200).json({ 'message': manyflips(req.params.number) })
 })
 
 app.use(function(req, res) {
-    res.status(404).send("Endpoint does not exist");
+    res.status(404).send("404 Not found");
     res.type("text/plain");
 })
 
