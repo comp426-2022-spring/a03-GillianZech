02ed8f0e6aedda58fe2e0f5400c9acc4540972c3
Gillian Zech <ginger01@email.unc.edu>
2022-03-17 16:15:12 -0400

    inserted functions

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port));
 })
 
-app.get('/app', (req, res) => {
+app.get('/app/', (req, res) => {
     res.status(200).end('OK, the API is working!');
     res.type("text/plain");
 })
 
-app.get('/app/echo/:number', (req, res) => {
+app.get('/app/flips/:number', (req, res) => {
+    const flips = manyflips(req.params.number)
     res.status(200).json({ 'message': req.params.number })
 })
 
@@ -20,3 +21,42 @@ app.use(function(req, res) {
     res.status(404).send("Endpoint does not exist");
     res.type("text/plain");
 })
+
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+}
+
+function coinFlips(flips) {
+    let flip_array = [];
+    while (flips > 0) {
+        flip_array.push(coinFlip());
+        flips--;
+    }
+    return flip_array;
+}
+
+
+function countFlips(array) {
+    let head = 0, tail = 0;
+    for (let item of array) {
+        if (item === "heads".valueOf()) {
+        head++;
+        }
+        else if (item === "tails".valueOf()) {
+        tail++;
+        }
+    }
+    return {heads: head, tails: tail};
+}
+
+function flipACoin(call) {
+    let flip_result = coinFlip(), win_result = 'lose';
+    if (flip_result == call) {
+        win_result = 'win'
+    }
+    return {
+        call: call,
+        flip: flip_result,
+        result: win_result 
+    };
+}
